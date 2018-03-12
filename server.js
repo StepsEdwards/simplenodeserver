ad9652fce7870a31aa9f7711613aa3829c7736c8
Stephens Jean-Jacques <stepsedwards@gmail.com>
2018-03-12 12:43:31 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+
+const mimeTypes = {
+    "html": "text/html",
+    "css": "text/css",
+    "js": "text/js",
+    "jpeg": "text/jpeg",
+    "jpg": "text/jpg",
+    "png": "text/png"
+}
+
+http.createServer((req, res) => {
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading' + uri);
+    var stats;
+
+    try {
+        stats = fs.lstatSync(fileName);
+    } catch (e) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+    
+    if(stats.isFile()){
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]]
+        res.writeHead(200, {'Content-Type': mimeType});
+    
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if(stats.isDirectory()){
+        res.writeHead(302, {
+            'Location': 'index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.write('500 Internal Error\n');
+        res.end();
+    }
+}).listen(1337);
+
